6bbf17d7c502616d953591066d210f32da04a6ed
Ali GÜNDOĞDU <ali@vuska.net>
2016-08-23 11:12:35 +0300

    Numarası olmayana işlem yapılmasın.

--- catalog/controller/module/bulutfon.php
+++ catalog/controller/module/bulutfon.php
@@ -1,25 +1,27 @@
 <?php
 
-class ControllerModuleBulutfon extends Controller {
+class ControllerModuleBulutfon extends Controller
+{
 
-    public function index() {
+    public function index()
+    {
 
         $action = $this->request->get["action"];
         $token = $this->request->get["token"];
 
         $securityKey = $this->config->get("bulutfon_secureKey");
 
-        if($securityKey != $token){
-            echo  json_encode(array(
-                'message'=>'no match secure key'
+        if ($securityKey != $token) {
+            echo json_encode(array(
+                'message' => 'no match secure key'
             ));
             die();
         }
 
-        if(method_exists($this,"action_$action")){
+        if (method_exists($this, "action_$action")) {
             $metod = "action_$action";
             $this->$metod();
-        }else{
+        } else {
             throw new Exception("Hatalı bilgiler");
         }
 
@@ -27,49 +29,55 @@ class ControllerModuleBulutfon extends Controller {
         die();
     }
 
-    private function action_cron(){
-        $sysDir = realpath(DIR_APPLICATION."/../system/bulutfon/");
-        include($sysDir."/autoload.php");
+    private function action_cron()
+    {
+        $sysDir = realpath(DIR_APPLICATION . "/../system/bulutfon/");
+        include($sysDir . "/autoload.php");
 
 
         $masterToken = $this->config->get("bulutfon_masterKey");
         $smsBaslik = $this->config->get("bulutfon_sms_baslik");
         $provider = new \Bulutfon\OAuth2\Client\Provider\Bulutfon(array(
-            'verifySSL'=>false
+            'verifySSL' => false
         ));
 
-        $ac = new \League\OAuth2\Client\Token\AccessToken(array('access_token'=>$masterToken));
+        $ac = new \League\OAuth2\Client\Token\AccessToken(array('access_token' => $masterToken));
 
         $cronCount = $this->config->get("ayar_sms_cronCount");
-        if(!$cronCount){$cronCount = 10;}
+        if (!$cronCount) {
+            $cronCount = 10;
+        }
 
-        $smsQs = $this->db->query("SELECT * FROM ". DB_PREFIX."sms_queue sq left join ".DB_PREFIX."sms_template st ON sq.template_id = st.id Where sq.status=1 order by st.id asc limit 0,10 ")->rows;
+        $smsQs = $this->db->query("SELECT * FROM " . DB_PREFIX . "sms_queue sq left join " . DB_PREFIX . "sms_template st ON sq.template_id = st.id Where sq.status=1 order by st.id asc limit 0,10 ")->rows;
 
 
-        foreach($smsQs as $sms){
-            $arguments = json_decode($sms["arguments"],true);
+        foreach ($smsQs as $sms) {
+            $arguments = json_decode($sms["arguments"], true);
             $content = $sms["content"];
-            foreach($arguments as $arKey => $arValue){
-                $content = str_replace("{$arKey}",$arValue,$content);
+            foreach ($arguments as $arKey => $arValue) {
+                $content = str_replace("{$arKey}", $arValue, $content);
             }
-            $m = array(
-                'title'=>$smsBaslik,
-                'receivers'=>'9'.$sms["phone_number"],
-                'content'=>$content
-            );
+            if (isset($sms["phone_number"]) && $sms["phone_number"]) {
+                $m = array(
+                    'title' => $smsBaslik,
+                    'receivers' => '9' . $sms["phone_number"],
+                    'content' => $content
+                );
 
-            $sonuc =(array) $provider->sendMessage($ac,$m);
-            if(isset($sonuc["message"]) && $sonuc["message"]=="Messages created successfully"){
-                $this->db->query("update ". DB_PREFIX."sms_queue set sms_content='$content',status=2");
+                $sonuc = (array)$provider->sendMessage($ac, $m);
+                if (isset($sonuc["message"]) && $sonuc["message"] == "Messages created successfully") {
+                    $this->db->query("update " . DB_PREFIX . "sms_queue set sms_content='$content',status=2");
+                }
             }
         }
         die();
     }
 
 
-    public function on_order_add($orderId) {
+    public function on_order_add($orderId)
+    {
 
-        if($this->config->get("bulutfon_notify_onOrderComplete")) {
+        if ($this->config->get("bulutfon_notify_onOrderComplete")) {
 
             $templateId = 1;
 
@@ -90,9 +98,10 @@ class ControllerModuleBulutfon extends Controller {
 
     }
 
-    public function on_order_history_add($orderId){
+    public function on_order_history_add($orderId)
+    {
 
-        if($this->config->get("bulutfon_notify_onOrderStatusChange")) {
+        if ($this->config->get("bulutfon_notify_onOrderStatusChange")) {
 
             $templateId = 2;
 
@@ -111,9 +120,10 @@ class ControllerModuleBulutfon extends Controller {
 
     }
 
-    public function on_customer_add($customerId){
+    public function on_customer_add($customerId)
+    {
 
-        if($this->config->get("bulutfon_notify_onNewUser")) {
+        if ($this->config->get("bulutfon_notify_onNewUser")) {
 
             $templateId = 3;
 
@@ -130,11 +140,12 @@ class ControllerModuleBulutfon extends Controller {
         }
     }
 
-    public function addQueue($phoneNumber, $templateId, $arguments){
+    public function addQueue($phoneNumber, $templateId, $arguments)
+    {
 
-        $rows = $this->db->query("SELECT * FROM ". DB_PREFIX."sms_queue WHERE date_added='".date('Y-m-d H:i:s')."' and template_id='".$templateId."' and phone_number='".$phoneNumber."'")->rows;
-        if(!$rows){
-            $this->db->query("INSERT INTO ".DB_PREFIX."sms_queue (date_added, status, arguments, template_id, phone_number) VALUES ( '".date('Y-m-d H:i:s')."', '1','".$arguments."','".$templateId."','".$phoneNumber."')");
+        $rows = $this->db->query("SELECT * FROM " . DB_PREFIX . "sms_queue WHERE date_added='" . date('Y-m-d H:i:s') . "' and template_id='" . $templateId . "' and phone_number='" . $phoneNumber . "'")->rows;
+        if (!$rows) {
+            $this->db->query("INSERT INTO " . DB_PREFIX . "sms_queue (date_added, status, arguments, template_id, phone_number) VALUES ( '" . date('Y-m-d H:i:s') . "', '1','" . $arguments . "','" . $templateId . "','" . $phoneNumber . "')");
         }
     }
 
